f91c4dd378edef537f6987fbc32c2a2048fb8fb3
root <root@ip-10-0-129-8.ec2.internal>
2023-03-10 10:15:55 +0000

    adding new code files

---
+++ customer/includes/db.php
@@ -0,0 +1,6 @@
+<?php
+$con = mysqli_connect("r-vpc-database-1.caav3m5vgnvl.us-east-1.rds.amazonaws.com", "admin", "admin123", "tagency");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
